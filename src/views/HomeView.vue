5a5231062177a28117945e7beab3a1e8407fdcc9
Eduard Mychka <edmychka@gmail.com>
2026-01-02 12:52:22 +0200

    feat: add people records section to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-vue-next';
+
+const handleAddPerson = () => {
+  console.log('Add Person');
+};
+</script>
 
 <template>
-  <main class="flex grow items-center justify-center p-6">
-    <h1>This is an about page</h1>
+  <main class="flex grow justify-center p-6">
+    <div class="w-full max-w-7xl">
+      <div class="mb-6 flex items-center justify-between">
+        <h2 class="text-2xl font-semibold">People records</h2>
+        <Button @click="handleAddPerson">
+          <Plus />
+          Add Person
+        </Button>
+      </div>
+    </div>
   </main>
 </template>
